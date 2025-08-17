982ff85caf3ad6459369df3402647335f5a54596
Kaushik R Bangera <kaushikrb909@gmail.com>
2025-08-17 21:25:10 +0530

    Enhance context module by exporting CtxError for improved error handling

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { TCtx, USER_ROLE, IBaseApi } from "./ctx/ctx.types";
 import { transformFromExpress } from "./transform/fromExpress";
 import { CtxRouter } from "./ctx/ctx.router";
-
+import { CtxError } from "./ctx/ctx.err";
 export type { TCtx, IBaseApi };
-export { USER_ROLE, CtxRouter };
+export { USER_ROLE, CtxRouter, CtxError };
 
 export namespace toCtx {
   export const fromExpress = transformFromExpress;
